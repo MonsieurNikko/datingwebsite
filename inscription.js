99f7224ef0cb89a98ffa2765146f6fbc2f611930
HUYNH Duc Duy <151777338+MonsieurNikko@users.noreply.github.com>
2024-05-16 17:12:51 +0200

    Update inscription.js

--- inscription.js
+++ inscription.js
@@ -1,185 +1,104 @@
-function countCharacters(event) {
-    var maxLength = 200;
-    var currentLength = event.target.value.length;
-    var caracteresElement = document.querySelector('.caracteres');
-    caracteresElement.textContent = currentLength + '/' + maxLength + ' caractères';
-    
-    if (currentLength > maxLength) {
-        caracteresElement.style.color = 'red'; // Optionnel: changer la couleur du texte lorsque la limite est atteinte
-    } 
-    else {
-        caracteresElement.style.color = '#888'; // Réinitialiser la couleur si le nombre de caractères est inférieur à la limite
+// Sélectionnez l'élément de l'introduction
+const introduction = document.getElementById('introduction');
+
+// Définissez le point de déclenchement (par exemple, 1000 pixels de défilement)
+const triggerPoint = 1000;
+
+// Fonction pour vérifier le défilement
+function checkScroll() {
+    if (window.scrollY > triggerPoint) {
+        // Ajoutez la classe "visible" pour afficher l'introduction
+        introduction.classList.add('visible');
+    } else {
+        // Retirez la classe "visible" pour cacher l'introduction
+        introduction.classList.remove('visible');
     }
 }
 
-function validateForm() {
-    var maxLength = 200;
-    var currentLength = document.getElementById('situation_amoureuse').value.length;
-    if (currentLength > maxLength) {
-        alert('La limite de caractères a été dépassée pour la situation amoureuse.');
-        return false; // Annuler l'envoi du formulaire si la limite est dépassée
+// Ajoutez un écouteur d'événement de défilement
+window.addEventListener('scroll', checkScroll);
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const connexionLink = document.getElementById('connexionlink');
+    const connexionForm = document.getElementById('connexionform');
+    const body = document.querySelector('body');
+
+    // Lorsque le formulaire de connexion est affiché
+    connexionLink.addEventListener('click', function(event) {
+        event.preventDefault();
+        connexionForm.style.display = 'block'; // Affiche le formulaire de connexion
+        body.classList.add('overlay-active'); // Ajoute la classe pour assombrir l'arrière-plan
+        document.getElementById('inscription').style.display = 'none'; // Masque la zone d'inscription
+    });
+});
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("connexionForm").addEventListener("submit", function(event) {
+        event.preventDefault(); // Empêche l'envoi du formulaire par défaut
+
+        var formData = new FormData(this); // Récupère les données du formulaire
+
+        // Envoie des données du formulaire à connexion.php via une requête AJAX
+        var xhr = new XMLHttpRequest();
+        xhr.open("POST", "connexion.php", true);
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState === XMLHttpRequest.DONE) {
+                // Vérifiez si le statut HTTP est 200 (OK)
+                if (xhr.status === 200) {
+                    // Affiche la réponse du serveur dans le div d'erreurMessage
+                    document.getElementById("erreurMessage").innerHTML = xhr.responseText;
+                } else {
+                    // Affiche une erreur générique si le statut HTTP n'est pas 200
+                    document.getElementById("erreurMessage").innerHTML = "Une erreur s'est produite.";
+                }
+            }
+        };
+        xhr.send(formData);
+    });
+});
+
+
+const header = document.querySelector('header');
+
+let prevscroll = window.scrollY;
+
+window.addEventListener('scroll', () => {
+    const currentscroll = window.scrollY;
+
+    if (currentscroll > prevscroll){
+        header.classList.add('hidden');
+    } else {
+        header.classList.remove('hidden');
     }
-    return true; // Permettre l'envoi du formulaire si la limite n'est pas dépassée
+
+    prevscroll = currentscroll;
+});
+
+
+
+
+let image = document.getElementById('userImage');
+let images = ['imagevisiteur.png', 'imagevisiteur2.png', 'imagevisiteur3.png', 'imagevisiteur4.png'];
+let currentIndex = 0;
+
+function changeImage() {
+    // Réduit l'opacité de l'image à zéro pour l'effet de transition sortant
+    image.style.opacity = 0;
+
+    // Utilise setTimeout pour changer l'image et remonter l'opacité après un court délai
+    setTimeout(() => {
+        // Change l'image source
+        image.src = images[currentIndex];
+        
+        // Remonte l'opacité à 1 pour l'effet de transition entrant
+        image.style.opacity = 1;
+
+        // Met à jour l'index pour la prochaine image
+        currentIndex = (currentIndex + 1) % images.length;
+    }, 500); // Délai égal à la durée de la transition d'opacité
 }
 
-function displaySelectedPhotos() {
-    var selectedPhotosDiv = document.getElementById('selected-photos');
-    selectedPhotosDiv.innerHTML = ''; // Clear previous selected photos
-
-    var files = document.getElementById('photos').files;
-    for (var i = 0; i < files.length; i++) {
-        var file = files[i];
-        var imageType = /^image\//;
-
-        if (!imageType.test(file.type)) {
-            continue; // Skip non-image files
-        }
-
-        var img = document.createElement('img');
-        img.classList.add('selected-photo');
-        img.file = file;
-        selectedPhotosDiv.appendChild(img);
-
-        var reader = new FileReader();
-        reader.onload = (function(aImg) {
-            return function(e) {
-                aImg.src = e.target.result;
-            };
-        })(img);
-        reader.readAsDataURL(file);
-    }
-}
-
-
-function validatePassword() {
-    var password = document.getElementById("mot_de_passe").value.trim();
-    var confirmPassword = document.getElementById("confirm_mot_de_passe").value.trim();
-    var passwordError = document.getElementById("password_error");
-
-    // Vérifie si les champs du mot de passe et de sa confirmation sont vides
-    if (password === "" || confirmPassword === "") {
-        passwordError.textContent = "Veuillez saisir votre mot de passe et le confirmer.";
-        return false;
-    }
-
-    // Vérifier si les mots de passe correspondent
-    if (password !== confirmPassword) {
-        passwordError.textContent = "Les mots de passe ne correspondent pas. Veuillez réessayer.";
-        return false;
-    }
-
-    // Vérifier la complexité du mot de passe
-    var passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()_+])[0-9a-zA-Z!@#$%^&*()_+]{12,27}$/;
-    
-    if (!passwordRegex.test(password)) {
-        passwordError.innerHTML = `Le mot de passe doit contenir au moins :<br>- un chiffre,<br>- une lettre minuscule,<br>- une lettre majuscule,<br>- une longueur entre 12 et 27 caractères.`;
-        return false;
-    }
-    
-    passwordError.textContent = ""; // Efface le message d'erreur s'il n'y a pas d'erreur
-    return true;
-}
-
-function toggleConfirmPasswordVisibility() {
-    var confirmPasswordInput1 = document.getElementById("mot_de_passe");
-    var confirmPasswordInput2 = document.getElementById("confirm_mot_de_passe");
-    var checkbox = document.getElementById("showConfirmPasswordCheckbox");
-
-    if (checkbox.checked) {
-        confirmPasswordInput1.type = "text";
-        confirmPasswordInput2.type = "text";
-    } 
-    else {
-        confirmPasswordInput1.type = "password";
-        confirmPasswordInput2.type = "password";
-    }
-    
-}
-
-// Initialisation de la première section
-let currentSection = 1;
-const sections = document.getElementsByClassName('section');
-
-function nextSection() {
-    // Valider le pseudonyme
-    var pseudonyme = document.getElementById("pseudonyme").value;
-    if (pseudonyme.trim() === "") {
-        alert("Veuillez saisir un pseudonyme.");
-        return; // Arrêter la fonction si le pseudonyme est vide
-    }
-
-    // Valider l'âge
-    var age = parseInt(document.getElementById("age").value);
-    if (isNaN(age) || age < 18 || age > 100) {
-        alert("Veuillez saisir un âge valide entre 18 et 100 ans.");
-        return; // Arrêter la fonction si l'âge n'est pas valide
-    }
-    
-    // Valider la date de naissance si elle est saisie
-    var dateNaissance = document.getElementById("date_naissance").value;
-    if (dateNaissance.trim() !== "") {
-        var selectedDate = new Date(dateNaissance);
-        var minDate = new Date("1924-01-01");
-        var maxDate = new Date("2006-12-31");
-
-        if (selectedDate < minDate || selectedDate > maxDate) {
-            alert("Veuillez sélectionner une date de naissance valide entre le 01/01/1924 et le 31/12/2006.");
-            return; // Arrêter la fonction si la date n'est pas valide
-        }
-    } 
-    else {
-        // Si la date de naissance n'est pas saisie, afficher un message et arrêter la fonction
-        alert("Veuillez saisir votre date de naissance.");
-        return;
-    }
-
-    // Valide le formulaire avant de passer à la section suivante
-    if (currentSection === 2 && !validateForm()) return; // Valide le formulaire de la première section
-    
-    if (currentSection === 3){
-    // Valider la taille
-      var taille = document.getElementById("taille").value.trim();
-      if (taille === "") {
-          alert("Veuillez saisir votre taille.");
-          return; // Arrêter la fonction si la taille n'est pas saisie
-      }
-      taille = parseInt(taille);
-      if (isNaN(taille) || taille < 100 || taille > 250) {
-          alert("Veuillez saisir une taille valide entre 100 et 250 cm.");
-          return; // Arrêter la fonction si la taille n'est pas valide
-      }
-
-      // Valider le poids
-      var poids = document.getElementById("poids").value.trim();
-      if (poids === "") {
-          alert("Veuillez saisir votre poids.");
-          return; // Arrêter la fonction si le poids n'est pas saisi
-      }
-      poids = parseInt(poids);
-      if (isNaN(poids) || poids < 20 || poids > 300) {
-          alert("Veuillez saisir un poids valide entre 20 et 300 kg.");
-          return; // Arrêter la fonction si le poids n'est pas valide
-      }
-    }
-    
-    if (currentSection === 4 && !validatePassword()) return; // Valide le mot de passe lors du passage à la quatrième section
-        currentSection++;
-
-    // Passe à la section suivante
-    var currentSectionElement = document.querySelector(".section:not([style='display: none;'])");
-    currentSectionElement.style.display = 'none'; // Masque la section actuelle
-    var nextSection = currentSectionElement.nextElementSibling;
-    if (nextSection) {
-        nextSection.style.display = 'block'; // Affiche la section suivante
-    } 
-    else {
-        document.getElementById('submitBtn').style.display = 'block'; // Affiche le bouton de soumission
-    }
-}
-
-function prevSection() {
-    if (currentSection === 1) return; // Arrête la fonction si nous sommes déjà à la première section
-    sections[currentSection - 1].style.display = 'none'; // Masque la section actuelle
-    currentSection--; // Décrémente la section actuelle
-    sections[currentSection - 1].style.display = 'block'; // Affiche la section précédente
-}
+// Appelle changeImage toutes les 1600 ms
+setInterval(changeImage, 3000);
