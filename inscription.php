0b6caacb3666af4f61dad78d51642b846ee4b151
zoroya95 <125991442+zoroya95@users.noreply.github.com>
2024-05-16 11:27:12 +0200

    Update inscription.php

--- inscription.php
+++ inscription.php
@@ -5,11 +5,20 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $age = $_POST['age'];
     $sexe = $_POST['sexe'];
     $date_naissance = $_POST['date_naissance'];
-    $profession = $_POST['profession'];
-
+    $residence = $_POST['lieu de résidence'];
+	$situation = $_POST['situation amoureuse'];
+	$taille = $_POST['taille'];
+	$poids = $_POST['poids'];
+	$cheveux = $_POST['couleur des cheveux'];
+	$yeux = $_POST['couleur des yeux'];
+	$nom = $_POST['nom'];
+	$adresse = $_POST['adresse'];
+	$password = $_POST['mot de passe'];
+	
+	
     // Écrire les données dans le fichier CSV
     $fichier_csv = 'csv/user.csv';
-    $nouvelle_ligne = "$pseudo,$age,$sexe,$date_naissance,$profession\n";
+    $nouvelle_ligne = "$pseudo,$age,$sexe,$date_naissance,$residence,$situation,$taille,$poids,$cheveux,$yeux,$nom,$adresse,$password\n";
     file_put_contents($fichier_csv, $nouvelle_ligne, FILE_APPEND);
 
     // Afficher un message de succès
