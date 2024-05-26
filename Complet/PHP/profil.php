94a864d3703bcaa95e05127df99c5e77c7544f66
KHELIL Yacine <133356569+NaTwiX27@users.noreply.github.com>
2024-05-26 21:53:53 +0200

    Update profil.php

--- Complet/PHP/profil.php
+++ Complet/PHP/profil.php
@@ -4,7 +4,7 @@ session_start();
 header('Content-Type: application/json');
 
 if (isset($_SESSION['pseudo']) && isset($_SESSION['data']) && isset($_SESSION['role'])) {
-    // Récupérez les données de l'utilisateur
+    // Récupérer les données de l'utilisateur
     $pseudo = $_SESSION['pseudo'];
     $data = $_SESSION['data'];
     $role = $_SESSION['role']; // Ajout du rôle de l'utilisateur
