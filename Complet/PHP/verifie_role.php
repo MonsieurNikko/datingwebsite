7ee5c28e5d060ad573688b22736431714f3b4283
KHELIL Yacine <133356569+NaTwiX27@users.noreply.github.com>
2024-05-26 23:06:33 +0200

    Update verifie_role.php

--- Complet/PHP/verifie_role.php
+++ Complet/PHP/verifie_role.php
@@ -1,12 +1,12 @@
 <?php
 session_start();
 
-header('Content-Type: application/json');
+header('Content-Type: application/json'); // Définir le type de contenu de la réponse comme JSON
 
 $response = array();
 
 if(isset($_SESSION['role'])) {
-    $response['role'] = $_SESSION['role'];
+    $response['role'] = $_SESSION['role']; // Ajouter le rôle de l'utilisateur à la réponse si défini
 }
 
 echo json_encode($response);
