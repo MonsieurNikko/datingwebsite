c8133f5f107f3a451bfeb20ab71379288ecaae50
KHELIL Yacine <133356569+NaTwiX27@users.noreply.github.com>
2024-05-26 13:51:14 +0200

    Update script.js

--- Complet/JavaScript/script.js
+++ Complet/JavaScript/script.js
@@ -12,12 +12,10 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
-
 document.addEventListener("DOMContentLoaded", function() {
     document.getElementById("connexionForm").addEventListener("submit", function(event) {
         event.preventDefault(); // Empêche l'envoi du formulaire par défaut
-        console.log("Formulaire soumis"); // Message de débogage
-
+        
         var formData = new FormData(this); // Récupère les données du formulaire
 
         // Envoie des données du formulaire à connexion.php via une requête AJAX
@@ -25,22 +23,17 @@ document.addEventListener("DOMContentLoaded", function() {
         xhr.open("POST", "../PHP/connexion.php", true);
         xhr.onreadystatechange = function() {
             if (xhr.readyState === XMLHttpRequest.DONE) {
-                console.log("Réponse reçue du serveur"); // Message de débogage
-
                 // Vérifiez si le statut HTTP est 200 (OK)
                 if (xhr.status === 200) {
-                    console.log("Statut HTTP 200"); // Message de débogage
-
                     // Redirige vers la page d'accueil après connexion réussie
                     window.location.href = "../HTML/accueil_apres_connexion.html";
-                } else if (xhr.status === 401) {
-                    console.log("Statut HTTP 401"); // Message de débogage
-
+                } 
+                else if (xhr.status === 401) {
                     // Affiche la réponse du serveur dans le div d'erreurMessage
                     document.getElementById("erreurMessage").innerHTML = xhr.responseText;
-                } else {
+                } 
+                else {
                     // Le traitement pour les autres erreurs HTTP
-                    console.log("Erreur HTTP: " + xhr.status); // Message de débogage
                     document.getElementById("erreurMessage").innerHTML = "Une erreur s'est produite.";
                 }
             }
@@ -48,4 +41,3 @@ document.addEventListener("DOMContentLoaded", function() {
         xhr.send(formData);
     });
 });
-
